f166b65832e18c8bfacacf74da126daa352d7826
Anwesh43 <anweshthecool0@gmail.com>
2021-07-12 19:00:06 +0530

    adding Canvas extension methods

--- triangledowntoleftview/src/main/java/com/example/triangledowntoleftview/TriangleDownToLeftView.kt
+++ triangledowntoleftview/src/main/java/com/example/triangledowntoleftview/TriangleDownToLeftView.kt
@@ -28,3 +28,43 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n)
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawTriangleDownToLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2 + (w / 2) * sc5, h / 2 + (h / 2) * (1 - sc3))
+    rotate(deg * sc4)
+    if (sc1 > 0f) {
+        drawLine(
+            -size / 2,
+            0f,
+            -size / 2 + (size / 2) * sc1,
+            -(size / 2) * sc1,
+            paint
+        )
+    }
+    if (sc2 > 0f) {
+        drawLine(
+            0f,
+            -size / 2,
+            (size / 2) * sc2,
+            -(size / 2) + size * 0.5f * sc1,
+            paint
+        )
+    }
+    restore()
+}
+
+fun Canvas.drawTDTLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTriangleDownToLeft(scale, w, h, paint)
+}

0d3ee88689c860234afbf59797768e99e20dee5e
Anwesh43 <anweshthecool0@gmail.com>
2021-07-12 19:01:29 +0530

    adding TriangleDownToLeftView class

--- triangledowntoleftview/src/main/java/com/example/triangledowntoleftview/TriangleDownToLeftView.kt
+++ triangledowntoleftview/src/main/java/com/example/triangledowntoleftview/TriangleDownToLeftView.kt
@@ -68,3 +68,19 @@ fun Canvas.drawTDTLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTriangleDownToLeft(scale, w, h, paint)
 }
+
+class TriangleDownToLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

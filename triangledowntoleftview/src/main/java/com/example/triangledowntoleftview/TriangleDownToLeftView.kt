0b7306e321c9294fa6b1215d6fc5297983c2ad94
Anwesh43 <anweshthecool0@gmail.com>
2021-07-12 20:06:04 +0530

    adding renderer in view

--- triangledowntoleftview/src/main/java/com/example/triangledowntoleftview/TriangleDownToLeftView.kt
+++ triangledowntoleftview/src/main/java/com/example/triangledowntoleftview/TriangleDownToLeftView.kt
@@ -71,14 +71,16 @@ fun Canvas.drawTDTLNode(i : Int, scale : Float, paint : Paint) {
 
 class TriangleDownToLeftView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -196,7 +198,7 @@ class TriangleDownToLeftView(ctx : Context) : View(ctx) {
         }
     }
 
-    data class Rederer(var view : TriangleDownToLeftView) {
+    data class Renderer(var view : TriangleDownToLeftView) {
 
         private val animator : Animator = Animator(view)
         private val tdl : TriangleDownToLeft = TriangleDownToLeft(0)

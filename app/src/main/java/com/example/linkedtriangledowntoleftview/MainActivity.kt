53d83cd96616ac7bb0d38276b47c6e17d6cd59c4
Anwesh43 <anweshthecool0@gmail.com>
2021-07-12 20:13:33 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedtriangledowntoleftview/MainActivity.kt
+++ app/src/main/java/com/example/linkedtriangledowntoleftview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedtriangledowntoleftview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.triangledowntoleftview.TriangleDownToLeftView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriangleDownToLeftView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
